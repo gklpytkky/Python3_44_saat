c3ecb11008253fc9b612e79b2a64134dc62b111e
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-12 16:30:20 +0300

    update

--- dictionary/dictionary-demo.py
+++ dictionary/dictionary-demo.py
@@ -30,5 +30,59 @@
     2- Öğrenci numarasını kullanıcıdan alıp ilgili öğrenci bilgisini gösteriniz.
 
 """
+# 1- cevap:
 
+ogrenciler = {}
 
+numara = input("Öğrenci no : ")
+isim = input("Öğrenci Adı : ")
+soyad = input("Öğrenci Soyadı : ")
+telefon = input("Öğrenci Telefonu : ")
+
+# ogrenciler[numara] = {
+#     "ad" : isim,
+#     "soyad" : soyad,
+#     "telefon" : telefon
+# }
+ogrenciler.update({
+    numara: {
+        "ad" : isim,
+        "soyad" : soyad,
+        "telefon" : telefon
+    }
+})
+
+numara = input("Öğrenci no : ")
+isim = input("Öğrenci Adı : ")
+soyad = input("Öğrenci Soyadı : ")
+telefon = input("Öğrenci Telefonu : ")
+
+ogrenciler.update({
+    numara: {
+        "ad": isim,
+        "soyad": soyad,
+        "telefon": telefon
+    }
+})
+
+numara = input("Öğrenci no : ")
+isim = input("Öğrenci Adı : ")
+soyad = input("Öğrenci Soyadı : ")
+telefon = input("Öğrenci Telefonu : ")
+
+ogrenciler.update({
+    numara: {
+        "ad": isim,
+        "soyad": soyad,
+        "telefon": telefon
+    }
+})
+
+print("*"*50)
+
+# 2- cevap:
+
+ogrNo = input("öğrenci no:")
+ogrenci = ogrenciler[ogrNo]
+print(ogrenci)
+print(f"Aradığınız {ogrNo} nolu öğrencinin adı: {ogrenci['ad']} soyadı: {ogrenci['soyad']} ve telefon ise {ogrenci['telefon']}")
