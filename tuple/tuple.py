fb25ef2590e2b26c365c2786721da911bee08996
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-12 14:02:57 +0300

    update

--- tuple/tuple.py
+++ tuple/tuple.py
@@ -10,3 +10,20 @@ print(tuple[2])
 
 print(len(tuple))
 print(len(liste))
+
+list = ["ali","veli"]
+tuple = ("damla","ayşe","ayşe")
+names = ("demet", "emel", "ayşe") + tuple
+
+print(names)
+
+list[0] = "ahmet"
+# tuple[0] = "deniz"
+
+print(tuple.count("ayşe"))
+print(tuple.index("ayşe"))
+
+
+
+print(list)
+print(tuple)
