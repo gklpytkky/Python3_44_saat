b095eca76f2402d09f13548b97b6fc14d4b1ac85
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-12 15:30:18 +0300

    Summary

---
+++ dictionary/dictionary-demo.py
@@ -0,0 +1,34 @@
+"""
+    ogrenciler =
+     {
+        
+        "120": 
+        {
+            "ad" : "Ali",
+            "soyad":"Yılmaz",
+            "telefon":"532 000 00 01"
+        },
+        
+        "125": 
+        {
+            "ad":"Can",
+            "soyad":"Korkmaz",
+            "telefon":"532 000 00 02"
+        },
+       
+        "128":
+        {
+            "ad":"Volkan",
+            "soyad":"Yükselen",
+            "telefon":"532 000 00 03"
+        },
+   
+    }
+
+    1- Bilgileri verilen öğrencileri kullanıcıdan aldığınız bilgilerle dictionary içinde saklayınız.
+
+    2- Öğrenci numarasını kullanıcıdan alıp ilgili öğrenci bilgisini gösteriniz.
+
+"""
+
+
