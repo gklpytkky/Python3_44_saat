f43cbcd72fc092e1577d25eebf85577bbe3f7ca3
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-13 15:14:45 +0300

    Summary

---
+++ comparison/comparison-demo.py
@@ -0,0 +1,4 @@
+# 1- Girilen 2 sayıdan hangisi büyüktür ?
+# 1- Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız. 
+#    Eger ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
+# 4-
