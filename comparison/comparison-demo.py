293b4c88328df64713ec4df015a75d8c557455ef
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-13 16:56:20 +0300

    Update

--- comparison/comparison-demo.py
+++ comparison/comparison-demo.py
@@ -1,4 +1,21 @@
 # 1- Girilen 2 sayıdan hangisi büyüktür ?
+a = input("1 sayı: ")
+b = input("2 sayı: ")
+buyuka = (a<b)
+buyukb = (a>b)
+resulta = (buyuka == True)
+print(resulta)
+resultb = (buyukb == True)
+print(resultb)
+
+
+
+
+
+
+
 # 1- Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız. 
 #    Eger ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
-# 4-
+# 4- Girilen bir sayının negatif pozitif durumunu yazdırın.
+# 5- Parola ve email bilgisini isteyip doğruluğunu kontrol ediniz.
+#    (email : email@sadikturan.com parola:abc123)
