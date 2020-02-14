8be1518fb9f242de7b8e83a03d502b487525b882
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-14 16:45:13 +0300

    Update

--- comparison/comparison-demo.py
+++ comparison/comparison-demo.py
@@ -1,20 +1,18 @@
 # 1- Girilen 2 sayıdan hangisi büyüktür ?
-a = input("1 sayı: ")
-b = input("2 sayı: ")
-buyuka = (a<b)
-buyukb = (a>b)
-resulta = (buyuka == True)
-print(resulta)
-resultb = (buyukb == True)
-print(resultb)
-
-
-
-
-
-
+# sayi1 = int(input("sayı gir: "))
+# sayi2 = int(input("sayı gir: "))
+# result = (sayi1 > sayi2)
+# print(f"{sayi1} değeri {sayi2} değerinden büyükmüdür : {result}")
 
 # 1- Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız. 
+vize1 = float(input("1.vize notu girin: "))
+vize2 = float(input("2.vize notu girin: "))
+final = float(input("final notu girin: "))
+
+a = ((vize1 + vize2) * 60 / 100) 
+b = (final * 40 / 100)
+print(a+b)
+
 #    Eger ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
 # 4- Girilen bir sayının negatif pozitif durumunu yazdırın.
 # 5- Parola ve email bilgisini isteyip doğruluğunu kontrol ediniz.
