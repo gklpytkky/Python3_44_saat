c7c0f8b01eaf796192558edca64cc72c35c21b9e
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-17 16:23:52 +0300

    Summary

---
+++ if-else/if-else.py
@@ -0,0 +1,12 @@
+username = "sadikturan"
+password = "1234"
+
+if (username == "sadikturan"):
+    
+    if (password == "1234"):
+        print("hoş geldiniz")
+    else:
+        print("parola yanlış")
+        
+else:
+    print("username yanlış")
