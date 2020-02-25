f1009fb10013400ff71d913e2d8f40f7da703a5d
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-25 14:57:32 +0300

    Update

--- loop-operators/loops-demo.py
+++ loop-operators/loops-demo.py
@@ -1,45 +1,34 @@
-
-# Sayı tutulup 3 hakta bulduran program
-# import random
-
-# tutulanSayi = random.randrange(1, 100)
-# print(tutulanSayi)
-
-# x = 1
-
-# while x < 4:
-#     tahmin = (int(input(f"{x}. Tahmininizi girin: ")))
-#     x += 1
-#     if tutulanSayi == tahmin:
-#         print("Sayıyı buldunuz.:D..")
-#     elif x == 4:
-#         print("Hakkınız bitti.!!.")
 """
     1-100 arasında rastgele üretilecek bir sayıyı aşağı yukarı ifadeleri ile buldurmaya çalışın. (hak = 5)
     ** "random modülü" için "python random" şeklinde arama yapın.
     ** 100 üzerinden puanlama yapın her soru 20 puan.
     ** Hak bilgisini kullanıcıdan alın ve her soru belirtilen can sayısı üzerinden hesaplansın.
 """
-# v2.0
 
+# Sayı tutulup 3 hakta bulduran program
 import random
 
 tutulanSayi = random.randrange(1, 100)
 print(tutulanSayi)
 
 x = 0
+tahmin = 0
 
-while x < 5:
-    tahmin = (int(input(f"Tahmininizi girin: ")))
+while x < 5 and tutulanSayi != tahmin:
     x += 1
-    if x == 5:
-       print("Hakkınız bitti..")
+    tahmin = (int(input(f"{x}.Tahminizi girin: ")))
+    if tutulanSayi == tahmin:
+        print("Bildiniz.")
     elif tutulanSayi < tahmin:
         print("Aşağı")
     elif tutulanSayi > tahmin:
         print("Yukarı")
-    elif tutulanSayi == tahmin:
-        print(f"Tebrikler tutulan {tutulanSayi} sayıyısını buldunuz..:D...")
+    if x == 5:
+        print("hakkınız bitti")
+
+
+
+    
     
 
 
