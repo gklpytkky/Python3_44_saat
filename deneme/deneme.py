75ab9ae820f81e203df6867f40df2c117d17a93d
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-02-27 16:26:37 +0300

    Create

---
+++ deneme/deneme.py
@@ -0,0 +1,27 @@
+# import random
+
+# def rastgeleSayi(sayi):
+#     print(sayi)    
+#     return
+
+# sayi = random.randint(1, 10)
+
+# rastgeleSayi(sayi)
+##############################################
+# def isimGir(isim,soyisim):
+#     print(isim,soyisim)
+#     return
+
+# isim = input("İsminizi giriniz: ")
+# soyisim = input("Soyisminizi giriniz: ")
+
+# isimGir(isim,soyisim)
+##############################################
+def sayiSay(say):
+    for i in range (1,10):
+        print(i)
+    return
+
+sayiSay(say)
+
+
