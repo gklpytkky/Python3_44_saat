458a72e366e512201572fb0c3fc02af66f2b3f7a
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-03-02 13:55:42 +0300

    Update

--- deneme/deneme.py
+++ deneme/deneme.py
@@ -17,11 +17,16 @@
 
 # isimGir(isim,soyisim)
 ##############################################
-def sayiSay(say):
-    for i in range (1,10):
-        print(i)
-    return
-
-sayiSay(say)
 
 
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
