46ff78dad628d9850cf3ddbfb75aa106979f6e40
Gökalp yatıkkaya <gokalpyatikkaya@hotmail.com>
2020-03-12 16:48:47 +0300

    Update

---
+++ test/test.py
@@ -0,0 +1,23 @@
+# def yaz():
+#     print("deneme")
+
+# yaz()
+############################
+# def toplam(a,b):
+#     c = a+b
+#     print(c)
+
+# toplam(5,6)
+#############################
+# def toplam():
+#     a = int(input("bir sayı girin: "))
+#     b = int(input("bir sayı girin: "))
+#     print(a+b)
+
+# toplam()
+#############################
+# for i in range(0,10+1):
+#     print(i)
+#############################
+
+
